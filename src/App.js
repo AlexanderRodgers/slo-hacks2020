37d69c21cfabd44b8a7670c9fa4fc8886d94a419
justintruong63 <56100380+justintruong63@users.noreply.github.com>
2020-02-29 03:20:05 -0800

    login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,129 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+
+        return (
+            <div className="root-container">
+
+                <div className="box-controller">
+                    <div className="controller">
+                        Login
+                    </div>
+                    <div className="controller">
+                        Register
+                    </div>
+                </div>
+            
+            
+
+                <div className="box-container">
+
+                    
+
+                
+                </div>
+            </div>
+        );
+    }
 }
 
-export default App;
+
+
+
+class LoginBox extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {   };
+    }
+
+    submitLogin(e){
+        
+    }
+
+    render() {
+        return(
+            <div className="inner-container">
+                <div className='header'>
+                  Login  
+                </div>
+
+                <div className="box">
+
+                    <div className="input-group">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" name="username" className="login-input" placeholder="Username"/>
+                    </div>
+
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" className="login-input" placeholder="Password"/>
+                    </div>
+
+                    <button type="button" className="login-btn" onClick={this.submitLogin.bind(this)}>Login</button>
+
+
+                </div>
+
+            </div>    
+        );
+        
+    }      
+        
+
+
+}
+
+class Register extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {   };
+    }
+
+    submitRegister(e){
+        
+    }
+
+    render() {
+        return(
+            <div className="inner-container">
+                <div className='header'>
+                  Register  
+                </div>
+
+                <div className="box">
+
+                    <div className="input-group">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" name="username" className="login-input" placeholder="Username"/>
+                    </div>
+
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" className="login-input" placeholder="Password"/>
+                    </div>
+
+                    <button type="button" className="login-btn" onClick={this.submitRegister.bind(this)}>Register</button>
+
+
+                </div>
+
+            </div>    
+        );
+        
+    }      
+        
+
+
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
